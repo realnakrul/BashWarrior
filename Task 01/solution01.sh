a21728b0dca7f5256598ffb91a62c65abfc66701
realnakrul <na_krul@mail.ru>
2021-04-13 09:27:11 +0400

    Task 01 Solution 01 updated, stderr redirected, comments added

--- Task 01/solution01.sh
+++ Task 01/solution01.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # Write a bash script which takes network interface name as input, checks the interface state and reports it
 
-STATE=$(ip addr show $1 | grep state)
+# if interface is not found stderr output will be redirected to null
+STATE=$(ip addr show $1 2>/dev/null | grep state)
+# if result of the previous command is 0 (success)
 if [ $? -eq 0 ]
 then
+    #Pattern matching
     STATE=${STATE#*state}
+    #Search *state from the beggining and remove
     STATE=${STATE%group*}
+    #Match group* from the end and remove
     echo Interface $1 is $STATE
 else
     echo Interface $1 not found
